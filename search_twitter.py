05d903caf569b5c305933d425c9f3c319012b36d
cactuss <cactuss.github@mailinator.com>
2011-10-12 01:41:52 -0400

    added 1 second timeout to twitter api

--- search_twitter.py
+++ search_twitter.py
@@ -9,7 +9,7 @@ def get_json(hash_tag):
   try:
     url = 'http://search.twitter.com/search.json?reputable=true&display_location=search-component&pc=true&q={hash_tag}&rpp=100'.format(hash_tag=hash_tag)
     r = urllib2.Request(url, '', { 'User-Agent' : "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11" })
-    twitter_json = urllib2.urlopen(r).readlines()[0]
+    twitter_json = urllib2.urlopen(r,timeout=1).readlines()[0]
     return json.decode(twitter_json)
   except urllib2.HTTPError:
     import time
@@ -21,6 +21,6 @@ def search_by_hashtags(hash_tag_search):
   # http://twitter.com/phoenix_search.phoenix?q=rt+heatmap&include_entities=1&include_available_features=1&contributor_details=true&filter=links
   url = 'http://twitter.com/phoenix_search.phoenix?q={hash_tag_search}&include_entities=1&include_available_features=1&contributor_details=true&filter=links'.format(hash_tag_search=hash_tag_search)
   r = urllib2.Request(url, '', { 'User-Agent' : "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11" })
-  twitter_json = json.decode(urllib2.urlopen(r).readlines()[0])
+  twitter_json = json.decode(urllib2.urlopen(r,timeout=1).readlines()[0])
   return twitter_json['statuses']
 
