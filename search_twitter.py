ffd7040cf59dd3304847232340e8aba874eef76d
cactuss <cactuss.github@mailinator.com>
2011-10-12 01:48:03 -0400

    last commit added dummy url used for tested, this commit reverts bug

--- search_twitter.py
+++ search_twitter.py
@@ -19,7 +19,7 @@ def get_json(hash_tag):
 def search_by_hashtags(hash_tag_search):
   # http://api.twitter.com/1/users/search.json?reputable=true&display_location=search-component&pc=true&q=rt+OR+heatmap
   # http://twitter.com/phoenix_search.phoenix?q=rt+heatmap&include_entities=1&include_available_features=1&contributor_details=true&filter=links
-  url = 'http://twitter.casdfom/phoenix_search.phoenix?q={hash_tag_search}&include_entities=1&include_available_features=1&contributor_details=true&filter=links'.format(hash_tag_search=hash_tag_search)
+  url = 'http://twitter.com/phoenix_search.phoenix?q={hash_tag_search}&include_entities=1&include_available_features=1&contributor_details=true&filter=links'.format(hash_tag_search=hash_tag_search)
   try:
     r = urllib2.Request(url, '', { 'User-Agent' : "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11" })
     twitter_json = json.decode(urllib2.urlopen(r,timeout=1).readlines()[0])
